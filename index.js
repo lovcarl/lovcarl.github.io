e73aba86c5f3b324e5fa0087ca3200c2db122987
lovcarl <lovcarl@users.noreply.github.com>
2019-04-05 13:49:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ var markers = [];
 
 
         vilmasPlace.addEventListener('click', function(event) {
-          placeMarker({lat: 59.35024615971273, lng: 18.058984491787896}, 59.35024615971273, 18.058984491787896);
-          console.log("vilmas!");
+          placeMarker(59.35024615971273, 18.058984491787896);
+          console.log("lovisa!");
         });
 
 
